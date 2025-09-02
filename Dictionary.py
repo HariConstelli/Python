5b9c2803524d52c5fe71bc868e0fd8f938583731
HariKrishna <harikrishna.k@constelli.com>
2025-09-02 19:01:01 +0530

    'Added dictionaries'

---
+++ Dictionary.py
@@ -0,0 +1,24 @@
+'''
+Python Dictionary is a data structure that stores the value in key:value pairs. Values in a dictionary can be of
+Any datatype and can be duplicated. Whereas Keys can't be repeated and must be immutable.
+
+
+a = {1:'Hari', 2:'Krishna', 3:1234567}
+print(a)
+
+d2 = dict(a = 'Hari', b = 'for', c = 'Krishna')
+print(d2)
+
+d = { "name": "Prajjwal", 1: "Python", (1, 2): [1,2,4] }
+print(d[1])
+
+d = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+
+# Adding a new key-value pair
+d["age"] = 22
+
+# Updating an existing value
+d[1] = "Python dict"
+
+print(d)
+'''
