92941c35cf426efd9bd7ea25764d6d3d4c955f98
HariKrishna <harikrishna.k@constelli.com>
2025-09-02 12:45:17 +0530

    'Added basic datatypes'

---
+++ Python_Basics.py
@@ -0,0 +1,47 @@
+#Python is a High level programming language invented by Guido Van Rossum in 1991
+
+#First Program In Python
+
+name = input('Enter your name:')
+print("Hi", name, 'Welcome to Python')
+
+#Variable : Variables acts a Placeholders for data
+
+age = 30
+'age is a variable which is assigned to that value 30'
+
+#Python Datatypes
+'''
+Numeric Datatypes: int(), float(), complex()
+
+a = 10
+print(int(a))
+print(float(a))
+print(complex(a))
+
+output 
+10
+10.0
+(10+0j)
+
+Sequence Type:  String, List, tuple
+
+String = 'String is a Sequence Data Type'
+
+List = [1,2,'1','b',30.1, 10+0j]
+
+tuple = (1,2,'a','b', 30)
+
+Mapping Datatype = dict()
+
+Dict is key, value pair = {1:10, 2:20, 3:'abc'}
+
+Boolean Datatype = bool
+
+#True or False
+
+#Set datatype
+
+set = {1,2,3,'a','b'}
+
+'''
